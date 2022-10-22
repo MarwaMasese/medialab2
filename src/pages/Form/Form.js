73621943607bd37e14f8e053417252223a370bce
SentiCryptdev <senticrypt@gmail.com>
2022-10-22 15:26:34 +0300

    Added Models and Form Page

---
+++ src/pages/Form/Form.js
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import './form.css'
+
+const WorkoutForm = () => {
+  const [title, setTitle] = useState('')
+  const [load, setLoad] = useState('')
+  const [reps, setReps] = useState('')
+  const [error, setError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const workout = {title, load, reps}
+    
+    const response = await fetch('/api/workouts', {
+      method: 'POST',
+      body: JSON.stringify(workout),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const json = await response.json()
+
+    if (!response.ok) {
+      setError(json.error)
+    }
+    if (response.ok) {
+      setError(null)
+      setTitle('')
+      setLoad('')
+      setReps('')
+      console.log('new workout added:', json)
+    }
+
+  }
+
+  return (
+    <form className="create" onSubmit={handleSubmit}> 
+      <h3>Add a New Workout</h3>
+
+      <label>Excersize Title:</label>
+      <input 
+        type="text" 
+        onChange={(e) => setTitle(e.target.value)} 
+        value={title}
+      />
+
+      <label>Description:</label>
+      <text 
+        type="text" 
+        onChange={(e) => setDesc(e.target.value)} 
+        value={desc}
+      />
+      {/* <label>Image:</label>
+      <input 
+        type="file" 
+        onChange={(e) => setImage(e.target.value)} 
+        value={file}
+      /> */}
+
+      <button>Add Workout</button>
+      {error && <div className="error">{error}</div>}
+    </form>
+  )
+}
+
+export default WorkoutForm
