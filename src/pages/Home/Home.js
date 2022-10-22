5c42b3bcc45bf3f6e47966c311bed9ee55912d28
SentiCryptdev <senticrypt@gmail.com>
2022-10-22 16:57:53 +0300

    Created an add post form

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,58 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../../Components/Navbar/Navbar'
+
 
 const Home = () => {
 	return (  
         
-		<body>
-  
-    <Navbar/>
-    <header>
-        {/* <!-- Header Start --> */}
-        <div class="header-area">
-                <div class="main-header ">
-                <div class="header-bottom  header-sticky">
-                        <div class="container">
-                            <div class="row align-items-center">
-                                
-                                <div class="col-xl-2 col-lg-2">
-                                    <div class="logo">
-                                        <a href="index.html"><img src="assets/img/logo/logo.png" alt=""/></a>
-                                    </div>
-                                </div>
-                                <div class="col-xl-10 col-lg-10">
-                                    <div class="menu-wrapper  d-flex align-items-center justify-content-end">
-                                        
-                                        <div class="main-menu d-none d-lg-block">
-                                            <nav> 
-                                                <ul id="navigation">                                                                                          
-                                                    <li><a href="index.html">Home</a></li>
-                                                    <li><a href="about.html">About</a></li>
-                                                    <li><a href="services.html">Services</a></li>
-                                                    <li><a href="blog.html">Blog</a>
-                                                        <ul class="submenu">
-                                                            <li><a href="blog.html">Blog</a></li>
-                                                            <li><a href="blog_details.html">Blog Details</a></li>
-                                                            <li><a href="elements.html">Element</a></li>
-                                                        </ul>
-                                                    </li>
-                                                    <li><a href="contact.html">Contact</a></li>
-                                                </ul>
-                                            </nav>
-                                        </div>
-                                    </div>
-                                </div> 
-                                {/* <!-- Mobile Menu --> */}
-                                <div class="col-12">
-                                    <div class="mobile_menu d-block d-lg-none"></div>
-                                </div>
-                            </div>
-                        </div>
-                </div>
-                </div>
-        </div>
-        {/* <!-- Header End --> */}
-    </header>
+<body>
     <main>
         <section class="blog_area section-padding pt-2">
             <div class="container">
